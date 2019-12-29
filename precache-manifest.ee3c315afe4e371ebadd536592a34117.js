4d625c92817987685e2cd14658955c40745f47a8
victimlisten <victimlisten@gmail.com>
2019-12-29 21:04:50 +0700

    Updates

---
+++ precache-manifest.ee3c315afe4e371ebadd536592a34117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ba31f6870abece8762cb55c8b0fa10",
+    "url": "/students-manager/index.html"
+  },
+  {
+    "revision": "92f4dc34b10771ca92ed",
+    "url": "/students-manager/static/css/main.02ae14b9.chunk.css"
+  },
+  {
+    "revision": "0c217c55c92f2f27e6de",
+    "url": "/students-manager/static/js/2.2cade251.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/students-manager/static/js/2.2cade251.chunk.js.LICENSE"
+  },
+  {
+    "revision": "92f4dc34b10771ca92ed",
+    "url": "/students-manager/static/js/main.1e0f84b7.chunk.js"
+  },
+  {
+    "revision": "64530e33ffffc3707a43",
+    "url": "/students-manager/static/js/runtime-main.2e4a5307.js"
+  }
+]);
